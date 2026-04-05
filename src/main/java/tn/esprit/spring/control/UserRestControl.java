e9ca72d0958bec00e78b06b38bf2f099fbdf8101
aminemezlini321 <131222273+aminemezlini321@users.noreply.github.com>
2026-04-05 13:22:10 +0100

    Update UserRestControl.java

--- src/main/java/tn/esprit/spring/control/UserRestControl.java
+++ src/main/java/tn/esprit/spring/control/UserRestControl.java
@@ -1,6 +1,4 @@
 package tn.esprit.spring.control;
-
-import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,10 +20,10 @@ public class UserRestControl {
 	@GetMapping("/retrieve-all-users")
 	public List<User> retrieveAllUsers() {
 		return userService.retrieveAllUsers();
-		//return list;
+		
 	}
  
-	// http://localhost:????/timesheet-devops/retrieve-user/{user-id}
+	
 	@GetMapping("/retrieve-user/{user-id}")
 	public User retrieveUser(@PathVariable("user-id") String userId) {
 		return userService.retrieveUser(userId);
@@ -42,7 +40,7 @@ public class UserRestControl {
 
 	
 	// Supprimer User : 
-	// http://localhost:????/timesheet-devops/remove-user/{user-id}
+	
 	@DeleteMapping("/remove-user/{user-id}") 
 	public void removeUser(@PathVariable("user-id") String userId) { 
 		userService.deleteUser(userId);
